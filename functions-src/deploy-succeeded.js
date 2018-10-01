cedfcb6d9eb4f3d99701bf84034cd74c8af0a6cd
Christian Allen <crhallen@gmail.com>
2018-10-01 19:39:27 +0100

    add environment variables

---
+++ functions-src/deploy-succeeded.js
@@ -0,0 +1,63 @@
+import Twitter from 'twitter'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const {
+  TWITTER_ACCESS_TOKEN_KEY,
+  TWITTER_ACCESS_TOKEN_SECRET,
+  TWITTER_CONSUMER_KEY,
+  TWITTER_CONSUMER_SECRET,
+} = process.env
+
+var client = new Twitter({
+  consumer_key: TWITTER_CONSUMER_KEY,
+  consumer_secret: TWITTER_CONSUMER_SECRET,
+  access_token_key: TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: TWITTER_ACCESS_TOKEN_SECRET,
+})
+
+exports.handler = function(event, context, callback) {
+  console.log('logging logging')
+  console.log(event)
+  console.log(context)
+
+  console.log(TWITTER_CONSUMER_SECRET)
+
+  // conditions:
+  // file must be called 'deploy-succeeded.js'
+  // (must be a POST)
+  // (must have valid jwt)
+  // must pass validation
+  // state = ready
+  // context = production
+  // title = "Create Blog "...
+
+  //need to extract url of post:  x.substring(
+  //   x.indexOf("“") + 1,
+  //     x.lastIndexOf("”")
+  // );
+
+  //const slug = title.substring(title.indexOf('“') + 1, title.lastIndexOf('”'))
+
+  // client.post(
+  //   'statuses/update',
+  //   {
+  //     status: `I just wrote a new post on my blog, check it out here: amandabrookfield.co.uk/blog/${slug}`,
+  //   },
+  //   function(error, tweet, response) {
+  //     if (error) return callback(error)
+  //     console.log(tweet) // Tweet body.
+  //     console.log(response) // Raw response object.
+
+  //     callback(null, {
+  //       statusCode: 200,
+  //       body: `Posted Successfully`,
+  //     })
+  //   }
+  // )
+
+  callback(null, {
+    statusCode: 200,
+    body: `Posted Successfully`,
+  })
+}
