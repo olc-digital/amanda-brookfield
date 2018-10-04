ce1d6c39deb8ba7d3a14399ec57492a8f0995a96
Christian Allen <crhallen@gmail.com>
2018-10-04 21:58:53 +0100

    digging deeper into event body

--- src/functions/deploy-succeeded.js
+++ src/functions/deploy-succeeded.js
@@ -27,10 +27,11 @@ exports.handler = async (event, context, callback) => {
   // Then get data from event.payload
   // Regular web-triggered functions get data from event.body
   try {
-    console.log(event.body.slice(0, event.body.length / 2))
-    const bod = JSON.stringify(event.body)
-    console.log(bod.slice(0, bod.length / 2))
-    console.log(JSON.parse(event.body))
+    // console.log(event.body.slice(0, event.body.length / 2))
+    // const bod = JSON.stringify(event.body)
+    // console.log(bod.slice(0, bod.length / 2))
+    // console.log(JSON.parse(event.body))
+    console.log(Object.keys(JSON.parse(event.body)))
     const {state, context, title} = JSON.parse(event.body)
     console.log('STATE:', state)
     console.log('CONTEXT:', context)
