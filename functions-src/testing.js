cdbc8509c0dee1e905bfe4c5a6a55959693a5e04
Christian Allen <crhallen@gmail.com>
2018-10-04 15:34:21 +0100

    remove state error

--- functions-src/testing.js
+++ functions-src/testing.js
@@ -20,7 +20,7 @@ exports.handler = function(event, context, callback) {
   console.log('logging logging')
   console.log(event)
   console.log(context)
-  console.log('state', event.payload.state)
+  console.log('state', event && event.payload && event.payload.state)
   console.log(TWITTER_CONSUMER_SECRET)
   console.log('hello')
   // conditions:
