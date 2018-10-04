c3f774c6e5b23cb55f36f0e46da7bfa319c2b0c9
Christian Allen <crhallen@gmail.com>
2018-10-04 20:15:19 +0100

    add twitter integration back in, with promises

--- src/functions/testing.js
+++ src/functions/testing.js
@@ -13,6 +13,7 @@ const titlePrefixCondition = 'Create Blog '
 const contextCondition = 'production'
 const stateCondition = 'ready'
 const urlPrefix = 'amandabrookfield.co.uk/blog'
+const tweetCopy = 'I just wrote a new post on my blog, check it out here:'
 
 const client = new Twitter({
   consumer_key: TWITTER_CONSUMER_KEY,
@@ -21,15 +22,8 @@ const client = new Twitter({
   access_token_secret: TWITTER_ACCESS_TOKEN_SECRET,
 })
 
-exports.handler = function(event, context, callback) {
-  // conditions:
+exports.handler = async (event, context, callback) => {
   // file must be called 'deploy-succeeded.js'
-  // (must be a POST)
-  // (must have valid jwt)
-  // must pass validation
-  // state = ready
-  // context = production
-  // title = "Create Blog "...
   try {
     const {state, context, title} = JSON.parse(event.body)
     console.log('>>> EVENT TYPE', state)
@@ -40,45 +34,24 @@ exports.handler = function(event, context, callback) {
       context !== contextCondition ||
       !title.startsWith(titlePrefixCondition)
     ) {
-      console.log('Non blogpost call to function')
+      console.log('Non blogpost-ready call to function')
       return callback(null, {
         statusCode: 200,
-        body: `Non blogpost call complete`,
+        body: `Non blogpost-ready call complete`,
       })
     }
     const slug = title.substring(title.indexOf('“') + 1, title.lastIndexOf('”'))
     const href = `${urlPrefix}/${slug}`
     console.log('href', href)
+    await client.post('statuses/update', {
+      status: `${tweetCopy} ${href}`,
+    })
+    callback(null, {
+      statusCode: 200,
+      body: `Tweeted Successfully`,
+    })
   } catch (err) {
-    console.log('failed to extract event details')
     console.log(err)
+    callback(err)
   }
-  //need to extract url of post:  x.substring(
-  //   x.indexOf("“") + 1,
-  //     x.lastIndexOf("”")
-  // );
-
-  //const slug = title.substring(title.indexOf('“') + 1, title.lastIndexOf('”'))
-
-  // client.post(
-  //   'statuses/update',
-  //   {
-  //     status: `I just wrote a new post on my blog, check it out here: amandabrookfield.co.uk/blog/${slug}`,
-  //   },
-  //   function(error, tweet, response) {
-  //     if (error) return callback(error)
-  //     console.log(tweet) // Tweet body.
-  //     console.log(response) // Raw response object.
-
-  //     callback(null, {
-  //       statusCode: 200,
-  //       body: `Posted Successfully`,
-  //     })
-  //   }
-  // )
-
-  callback(null, {
-    statusCode: 200,
-    body: `Posted Successfully`,
-  })
 }
