b4e23f1d9215ce53e3eaab42ee69e09e8f25cbe6
Christian Allen <crhallen@gmail.com>
2018-10-04 16:01:29 +0100

    move functions into src folder and install all dependencies at top level

--- src/functions/testing.js
+++ src/functions/testing.js
@@ -1,6 +1,6 @@
-// import Twitter from 'twitter'
-// import dotenv from 'dotenv'
-// dotenv.config()
+import Twitter from 'twitter'
+import dotenv from 'dotenv'
+dotenv.config()
 
 const {
   TWITTER_ACCESS_TOKEN_KEY,
@@ -9,12 +9,12 @@ const {
   TWITTER_CONSUMER_SECRET,
 } = process.env
 
-// var client = new Twitter({
-//   consumer_key: TWITTER_CONSUMER_KEY,
-//   consumer_secret: TWITTER_CONSUMER_SECRET,
-//   access_token_key: TWITTER_ACCESS_TOKEN_KEY,
-//   access_token_secret: TWITTER_ACCESS_TOKEN_SECRET,
-// })
+var client = new Twitter({
+  consumer_key: TWITTER_CONSUMER_KEY,
+  consumer_secret: TWITTER_CONSUMER_SECRET,
+  access_token_key: TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: TWITTER_ACCESS_TOKEN_SECRET,
+})
 
 exports.handler = function(event, context, callback) {
   console.log('logging logging')
