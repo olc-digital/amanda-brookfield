de234620520d4ee4454924a21822743f0f3eeafa
Christian Allen <crhallen@gmail.com>
2018-10-04 15:30:25 +0100

    remove main site build and twitter import

--- functions-src/testing.js
+++ functions-src/testing.js
@@ -1,4 +1,4 @@
-import Twitter from 'twitter'
+// import Twitter from 'twitter'
 // import dotenv from 'dotenv'
 // dotenv.config()
 
@@ -9,12 +9,12 @@ const {
   TWITTER_CONSUMER_SECRET,
 } = process.env
 
-var client = new Twitter({
-  consumer_key: TWITTER_CONSUMER_KEY,
-  consumer_secret: TWITTER_CONSUMER_SECRET,
-  access_token_key: TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: TWITTER_ACCESS_TOKEN_SECRET,
-})
+// var client = new Twitter({
+//   consumer_key: TWITTER_CONSUMER_KEY,
+//   consumer_secret: TWITTER_CONSUMER_SECRET,
+//   access_token_key: TWITTER_ACCESS_TOKEN_KEY,
+//   access_token_secret: TWITTER_ACCESS_TOKEN_SECRET,
+// })
 
 exports.handler = function(event, context, callback) {
   console.log('logging logging')
