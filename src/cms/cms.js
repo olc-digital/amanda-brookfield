69db8876a27f1be2c47fdc2e2b2b1dc049c474bc
crhallen <7114281+crhallen@users.noreply.github.com>
2019-05-16 22:21:45 +0100

    remove product page stuff

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,14 +1,12 @@
 import CMS from 'netlify-cms-app'
 import {withLayout, withStyledSheets} from './wrappers'
 
-import ProductPagePreview from './preview-templates/ProductPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ArticlePreview from './preview-templates/ArticlePreview'
 import BookPreview from './preview-templates/BookPreview'
 import EventsPreview from './preview-templates/EventsPreview'
 import AnnouncementPreview from './preview-templates/AnnouncementPreview'
 
-CMS.registerPreviewTemplate('products', withStyledSheets(ProductPagePreview))
 CMS.registerPreviewTemplate('blog', withLayout(BlogPostPreview))
 CMS.registerPreviewTemplate('articles', withLayout(ArticlePreview))
 CMS.registerPreviewTemplate('books', withLayout(BookPreview))
