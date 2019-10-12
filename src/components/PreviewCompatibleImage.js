a7c8d79271eecd8d76f442b9eec85761754c660e
crhallen <7114281+crhallen@users.noreply.github.com>
2019-10-12 12:48:53 +0100

    Fix book page image display and add other fields to CMS

--- src/components/PreviewCompatibleImage.js
+++ src/components/PreviewCompatibleImage.js
@@ -2,22 +2,48 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Img from 'gatsby-image'
 
-const PreviewCompatibleImage = ({imageInfo}) => {
-  const imageStyle = {borderRadius: '5px'}
-  const {alt = '', childImageSharp, image} = imageInfo
+const ImgFluidOrFixed = ({style, childImageSharp, alt}) => {
+  if (childImageSharp.fluid) {
+    return <Img style={style} fluid={childImageSharp.fluid} alt={alt} />
+  }
+
+  if (childImageSharp.fixed) {
+    return <Img style={style} fixed={childImageSharp.fixed} alt={alt} />
+  }
+
+  return null
+}
+
+const PreviewCompatibleImage = ({imageInfo, style}) => {
+  if (!imageInfo) {
+    return null
+  }
+
+  const {alt = '', childImageSharp, value: image} = imageInfo
 
   if (!!image && !!image.childImageSharp) {
     return (
-      <Img style={imageStyle} fluid={image.childImageSharp.fluid} alt={alt} />
+      <ImgFluidOrFixed
+        style={style}
+        childImageSharp={image.childImageSharp}
+        alt={alt}
+      />
     )
   }
 
   if (childImageSharp) {
-    return <Img style={imageStyle} fluid={childImageSharp.fluid} alt={alt} />
+    return (
+      <ImgFluidOrFixed
+        style={style}
+        childImageSharp={childImageSharp}
+        alt={alt}
+      />
+    )
   }
 
-  if (!!image && typeof image === 'string')
-    return <img style={imageStyle} src={image} alt={alt} />
+  if (!!image && typeof image === 'string') {
+    return <img style={style} src={image} alt={alt} />
+  }
 
   return null
 }
@@ -26,7 +52,7 @@ PreviewCompatibleImage.propTypes = {
   imageInfo: PropTypes.shape({
     alt: PropTypes.string,
     childImageSharp: PropTypes.object,
-    image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]).isRequired,
+    image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
     style: PropTypes.object,
   }).isRequired,
 }
