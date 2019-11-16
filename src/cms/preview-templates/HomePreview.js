d4406062ce1ebef6dcac8682bd651fa4e7cd297d
crhallen <7114281+crhallen@users.noreply.github.com>
2019-11-16 12:56:16 +0000

    add comment about fieldsMetaData where relation data will come from

--- src/cms/preview-templates/HomePreview.js
+++ src/cms/preview-templates/HomePreview.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import {HomePageTemplate} from '../../templates/home-page'
 
-const HomePreview = ({entry}) => {
+const HomePreview = ({entry, fieldsMetaData}) => {
+  console.log('entry', entry.toJS())
+  console.log('meta', fieldsMetaData.toJS()) //For example, the Relation widget passes the whole selected relation data in fieldsMetaData.
   return <HomePageTemplate welcomeText={entry.getIn(['data', 'welcomeText'])} />
 }
 
