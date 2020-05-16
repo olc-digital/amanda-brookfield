2f2ebca43042d8addafa7dfe139bac5f5756a5a3
Christian Allen <7114281+crhallen@users.noreply.github.com>
2020-05-16 15:51:03 +0100

    Preset Mabel images, plus mabel page description (#52)
    * Add preset mabel images
    
    * Fix CMS preview
    
    * add description and bottom CTA to mabel page
    
    * add description onto live site
    
    * Update Author “mabel”
    
    * Update Author “mabel”
    
    * Add facebook cover image assets
    
    * update CMS to point to master again

--- src/cms/preview-templates/MabelPreview.js
+++ src/cms/preview-templates/MabelPreview.js
@@ -2,17 +2,34 @@ import React from 'react'
 import {MabelPageTemplate} from '../../templates/mabel-page'
 import instaPlaceholder from '../../img/insta-placeholder.gif'
 
-const MabelPreview = ({entry, getAsset}) => (
-  <MabelPageTemplate
-    title={entry.getIn(['data', 'title'])}
-    metaDescription={entry.getIn(['data', 'metaDescription'])}
-    inspirationImage={getAsset(entry.getIn(['data', 'inspirationImage']))}
-    biographyImage={getAsset(entry.getIn(['data', 'biographyImage']))}
-    mabelImage={getAsset(entry.getIn(['data', 'mabelImage']))}
-    images={new Array(12)
-      .fill(instaPlaceholder)
-      .map((url, i) => ({node: {id: i, localFile: {url}}}))}
-  />
-)
+const MabelPreview = ({entry, getAsset}) => {
+  const {data} = entry.toJS()
+
+  const presetGalleryImage1 = {
+    id: data.presetGalleryImage1.id,
+    path: getAsset(data.presetGalleryImage1.path),
+  }
+
+  const presetGalleryImage2 = {
+    id: data.presetGalleryImage2.id,
+    path: getAsset(data.presetGalleryImage2.path),
+  }
+
+  return (
+    <MabelPageTemplate
+      title={entry.getIn(['data', 'title'])}
+      metaDescription={entry.getIn(['data', 'metaDescription'])}
+      description={entry.getIn(['data', 'description'])}
+      inspirationImage={getAsset(entry.getIn(['data', 'inspirationImage']))}
+      biographyImage={getAsset(entry.getIn(['data', 'biographyImage']))}
+      mabelImage={getAsset(entry.getIn(['data', 'mabelImage']))}
+      presetGalleryImage1={presetGalleryImage1}
+      presetGalleryImage2={presetGalleryImage2}
+      instagramImages={new Array(12)
+        .fill(instaPlaceholder)
+        .map((url, i) => ({node: {id: i, localFile: {url}}}))}
+    />
+  )
+}
 
 export default MabelPreview
--- src/templates/mabel-page.js
+++ src/templates/mabel-page.js
@@ -16,8 +16,6 @@ import Img from '../components/PreviewCompatibleImage'
 const InstagramCTA = styled.div`
   ${kalamFont}
   text-align: center;
-  margin-bottom: 48px;
-  margin-top: 16px;
   a {
     text-decoration: none !important;
   }
@@ -31,6 +29,7 @@ const Grid = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(180px, 1fr));
   gap: 16px;
+  margin: 48px 0;
 `
 
 const SquareImageWrapper = styled.div`
@@ -50,14 +49,32 @@ const SquareImageWrapper = styled.div`
   }
 `
 
+const Description = styled.div`
+  margin: 12px auto 32px;
+  text-align: center;
+  max-width: 400px;
+`
+
 export const MabelPageTemplate = ({
   title,
-  images,
   metaDescription,
+  description,
+  instagramImages = [],
   inspirationImage,
   biographyImage,
   mabelImage,
+  presetGalleryImage1,
+  presetGalleryImage2,
 }) => {
+  const images = [
+    {id: presetGalleryImage1.id, localFile: presetGalleryImage1.path},
+    {id: presetGalleryImage2.id, localFile: presetGalleryImage2.path},
+    ...instagramImages
+      .map(({node}) => node)
+      .sort((a, b) => b.timestamp - a.timestamp)
+      .slice(0, 10),
+  ]
+
   return (
     <Page>
       <HelmetHelper title={title} metaDescription={metaDescription} />
@@ -70,6 +87,7 @@ export const MabelPageTemplate = ({
         <H2 margin style={{marginBottom: 0}}>
           {title}
         </H2>
+        <Description>{description}</Description>
         <InstagramCTA>
           <ExternalLink href="https://www.instagram.com/amanda_and_mabel_brookfield/">
             <div>Find us on Instagram!</div>
@@ -80,18 +98,26 @@ export const MabelPageTemplate = ({
           </ExternalLink>
         </InstagramCTA>
         <Grid>
-          {Array.isArray(images) &&
-            images.map(({node: image}) => (
-              <ExternalLink
-                href={`https://www.instagram.com/p/${image.id}`}
-                key={image.id}
-              >
-                <SquareImageWrapper>
-                  <Img imageInfo={image.localFile} />
-                </SquareImageWrapper>
-              </ExternalLink>
-            ))}
+          {images.map(image => (
+            <ExternalLink
+              href={`https://www.instagram.com/p/${image.id}`}
+              key={image.id}
+            >
+              <SquareImageWrapper>
+                <Img style={{objectFit: 'cover'}} imageInfo={image.localFile} />
+              </SquareImageWrapper>
+            </ExternalLink>
+          ))}
         </Grid>
+        <InstagramCTA>
+          <ExternalLink href="https://www.instagram.com/amanda_and_mabel_brookfield/">
+            <div>See more on Instagram</div>
+            <FontAwesomeIcon
+              className="mabel-insta-icon"
+              icon={['fab', 'instagram']}
+            />
+          </ExternalLink>
+        </InstagramCTA>
       </Container>
     </Page>
   )
@@ -100,17 +126,20 @@ export const MabelPageTemplate = ({
 const MabelPage = ({data}) => {
   const {
     markdownRemark: post,
-    allInstaNode: {edges: images},
+    allInstaNode: {edges: instagramImages},
   } = data
 
   return (
     <MabelPageTemplate
       title={post.frontmatter.title}
+      metaDescription={post.frontmatter.metaDescription}
+      description={post.frontmatter.description}
       inspirationImage={post.frontmatter.inspirationImage}
       biographyImage={post.frontmatter.biographyImage}
       mabelImage={post.frontmatter.mabelImage}
-      images={images}
-      metaDescription={post.frontmatter.metaDescription}
+      instagramImages={instagramImages}
+      presetGalleryImage1={post.frontmatter.presetGalleryImage1}
+      presetGalleryImage2={post.frontmatter.presetGalleryImage2}
     />
   )
 }
@@ -124,6 +153,7 @@ export const mabelPageQuery = graphql`
       frontmatter {
         title
         metaDescription
+        description
         inspirationImage {
           childImageSharp {
             fluid {
@@ -142,6 +172,26 @@ export const mabelPageQuery = graphql`
           childImageSharp {
             fluid {
               ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        presetGalleryImage1 {
+          id
+          path {
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid_tracedSVG
+              }
+            }
+          }
+        }
+        presetGalleryImage2 {
+          id
+          path {
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid_tracedSVG
+              }
             }
           }
         }
